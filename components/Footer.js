8bfebefaa13e33ca7b75c86877764ef0ba915c81
Rami1125 <rami.msarwa1@gmail.com>
2026-07-14 08:03:01 +0300

    Add Footer component with copyright information

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-6 text-center">
+      © 2026 ח. סבן חומרי בניין (1994) בע"מ | סניפי הוד השרון
+    </footer>
+  );
+}
